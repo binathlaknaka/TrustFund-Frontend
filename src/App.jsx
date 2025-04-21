e731cb3dbca465ac9a620cb10566fabce65f5cff
binathlaknaka <binalakiya.11@gmail.com>
2025-04-21 14:29:18 +0000

    Navbar Initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import Home from './pages/Home';
-import About from './pages/About';
+
+const helmetContext = {};
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <HelmetProvider context={helmetContext}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </HelmetProvider>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import Button from '../components/Button';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Welcome</h1>
-      <Button>Click Me</Button>
+    <div className="App">
+      <Navbar />
     </div>
   );
 }
