a12023b6986013925985e09bed9e517fb1f09987
binathlaknaka <binalakiya.11@gmail.com>
2025-04-21 13:16:39 +0000

    React Dom Setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Button from '../components/Button';
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Welcome</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
